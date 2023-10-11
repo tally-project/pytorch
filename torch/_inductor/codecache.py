37bf37a878d1dc9c044ceb4093aa4948674a1e45
wzhao18 <wzhao18.sz@gmail.com>
2023-10-11 06:04:58 -0400

    Make default torch inductor cache dir in .cache

--- torch/_inductor/codecache.py
+++ torch/_inductor/codecache.py
@@ -108,7 +108,7 @@ log = logging.getLogger(__name__)
 def cache_dir() -> str:
     cache_dir = os.environ.get("TORCHINDUCTOR_CACHE_DIR")
     if cache_dir is None:
-        cache_dir = f"{tempfile.gettempdir()}/torchinductor_{getpass.getuser()}"
+        cache_dir = f"{str(Path.home())}/.cache/torchinductor_{getpass.getuser()}"
     os.makedirs(cache_dir, exist_ok=True)
     return cache_dir
 
